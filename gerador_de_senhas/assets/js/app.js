563b4c5af19ecd6b4a37e5219d783f7140b2090c
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-19 13:51:33 -0300

    class 105 - finishing password generation

--- gerador_de_senhas/assets/js/app.js
+++ gerador_de_senhas/assets/js/app.js
@@ -1,7 +1,7 @@
 /* 
     elements selection 
 */
-const generate_password_element = document.querySelector('[data_generated_password]');
+const generated_password_element = document.querySelector('[data_generated_password]');
 const generate_password_button = document.querySelector('[data_generate_password]');
 
 /* 
@@ -28,11 +28,29 @@ const getSymbol = () => {
     return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
-console.log(getSymbol());
+// 
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+    let password = '';
+    const password_length = 10;
+
+    const generators = [getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol];
+
+    for (i = 0; i < password_length; i = i + generators.length) {
+        generators.forEach(() => {
+            const random_value = generators[Math.floor(Math.random() * generators.length)]();
+            password += random_value;
+        })
+    };
+
+    password = password.slice(0, password_length);
+
+    generated_password_element.style.display = 'block';
+    generated_password_element.querySelector('h4').innerText = password;
+};
 
 /* 
     events 
 */
 generate_password_button.addEventListener('click', () => {
-    console.log('teste');
+    generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol);
 });
